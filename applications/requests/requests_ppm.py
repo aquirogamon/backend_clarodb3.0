0bbc4c902661efbcde5fb0255df7bbffc33f1657
Armando Quiroga <aquirogamon@gmail.com>
2020-02-01 11:12:26 -0500

    UPdate ANY

--- applications/requests/requests_ppm.py
+++ applications/requests/requests_ppm.py
@@ -22,10 +22,10 @@ def dataInternetInterface():
         fqdn = '&FQDN=Node%3D{}'
         url = 'https://aquiroga:Cl4r0peru51@172.19.212.8:4440/ppm/rest/reports/Transport+Statistics/Interface/Interface+Utilization%2FBit+Rates?outputType=jsonv2&durationSelect=lastHour&intervalTypeKey=FIVE_MINUTE&maxPageSize=200000' + fqdn
         responseData = requests.get(url.format(ip_address), verify=False)
-        responseDataJSON = requests.get(url.format(ip_address), verify=False).json()["report"]["data"]
-        for interface in responseDataJSON:
-            responseDataCode = responseData.status_code
-            if responseDataCode == 200:
+        responseDataCode = responseData.status_code
+        if responseDataCode == 200:
+            responseDataJSON = requests.get(url.format(ip_address), verify=False).json()["report"]["data"]
+            for interface in responseDataJSON:
                 deviceIndex = device['name'] + interface[3]
 
                 if float(interface[8].replace(',', '')) > float(interface[7].replace(',', '')):
@@ -82,10 +82,10 @@ def dataCoreInterface():
         fqdn = '&FQDN=Node%3D{}'
         url = 'https://aquiroga:Cl4r0peru51@172.19.212.8:4440/ppm/rest/reports/Transport+Statistics/Interface/Interface+Utilization%2FBit+Rates?outputType=jsonv2&durationSelect=lastHour&intervalTypeKey=FIVE_MINUTE&maxPageSize=200000' + fqdn
         responseData = requests.get(url.format(ip_address), verify=False)
-        responseDataJSON = requests.get(url.format(ip_address), verify=False).json()["report"]["data"]
-        for interface in responseDataJSON:
-            responseDataCode = responseData.status_code
-            if responseDataCode == 200:
+        responseDataCode = responseData.status_code
+        if responseDataCode == 200:
+            responseDataJSON = requests.get(url.format(ip_address), verify=False).json()["report"]["data"]
+            for interface in responseDataJSON:
                 deviceIndex = device['name'] + interface[3]
 
                 if float(interface[8].replace(',', '')) > float(interface[7].replace(',', '')):
